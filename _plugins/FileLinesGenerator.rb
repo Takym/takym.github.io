4dc1f035c7e8b58f10384f700fc96e372dd0bc50
Takym <15681312+Takym@users.noreply.github.com>
2024-11-28 23:07:16 +0900

    プラグインインストール

--- _plugins/FileLinesGenerator.rb
+++ _plugins/FileLinesGenerator.rb
@@ -2,8 +2,11 @@ module TakymGitHubIO
 	class FileLinesGenerator < Jekyll::Generator
 		safe true
 		def generate(site)
+			site.pages.each do |page|
+				page.data['file_lines'] = File.read(page.name).lines.count
+			end
 			site.posts.each do |post|
-				post.data['file_lines'] = File.readlines(post.name).size
+				post.data['file_lines'] = File.read(post.name).lines.count
 			end
 		end
 	end
